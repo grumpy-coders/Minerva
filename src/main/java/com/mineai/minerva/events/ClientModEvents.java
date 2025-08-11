889ab7dad9a17177fc99cf6aa6c84da6407340b6
Tom Rooker <2650021+LordDurus@users.noreply.github.com>
2025-08-10 20:38:06 -0400

    runs in terminal with start gradlew.bat runClient

--- src/main/java/com/mineai/minerva/events/ClientModEvents.java
+++ src/main/java/com/mineai/minerva/events/ClientModEvents.java
@@ -1,24 +1,31 @@
-package com.mineai.minerva.events;
-
-import com.mineai.minerva.MinervaMod;
-import com.mineai.minerva.client.MinervaRenderer;
-import com.mineai.minerva.entity.MinervaEntity;
-import com.mineai.minerva.entity.ModEntities;
-
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = MinervaMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class ClientModEvents {
-
-	@SubscribeEvent
-	public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-		event.registerEntityRenderer(ModEntities.MINERVA_ENTITY.get(), MinervaRenderer::new);
-	}
-	
-	@SubscribeEvent
-	public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
-		event.registerLayerDefinition(Minerva, MinervaRenderer::new);
-	}
-}
+/*
+ * package com.mineai.minerva.events;
+ * 
+ * import com.mineai.minerva.MinervaMod;
+ * import com.mineai.minerva.client.MinervaRenderer;
+ * import com.mineai.minerva.entity.MinervaEntity;
+ * import com.mineai.minerva.entity.ModEntities;
+ * 
+ * import net.minecraftforge.client.event.EntityRenderersEvent;
+ * import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+ * import net.minecraftforge.fml.common.Mod;
+ * 
+ * @Mod.EventBusSubscriber(modid = MinervaMod.MODID, bus =
+ * Mod.EventBusSubscriber.Bus.MOD)
+ * public class ClientModEvents {
+ * 
+ * @SubscribeEvent
+ * public static void registerRenderers(EntityRenderersEvent.RegisterRenderers
+ * event) {
+ * event.registerEntityRenderer(ModEntities.MINERVA_ENTITY.get(),
+ * MinervaRenderer::new);
+ * }
+ * 
+ * @SubscribeEvent
+ * public static void
+ * registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event)
+ * {
+ * event.registerLayerDefinition(Minerva, MinervaRenderer::new);
+ * }
+ * }
+ */
